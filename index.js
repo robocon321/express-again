47333d74958041a0bf00a482781abbf709346f7b
robocon321 <robocon321n@gmail.com>
2020-01-12 23:13:22 +0700

    Add index.js & users.pug

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app=express();
 
 var port=3000;
 
+var users=[
+			{name:"Hong 10",phone:"0354334442"},
+			{name:"Wing",phone:"0923839923"},
+			{name:"Skim",phone:"0394983959"}
+		];
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -13,15 +19,21 @@ app.get('/',function(req,res){
 });
 
 app.get('/users',function(req,res){
-	res.render('users',{
-		users:[
-			{name:"Hong 10",phone:"0354334442"},
-			{name:"Wing",phone:"0923839923"},
-			{name:"Skim",phone:"0394983959"}
-		]
-	})
+	res.render('users',{users:users})
 })
 
+app.get('/users/search',function(req,res){
+	var q=req.query.q;
+	console.log(q);
+	var arr=users.filter(function(item){
+		return item.name.toLowerCase().indexOf(q.toLowerCase())>=0;
+	});
+	res.render('users/search',{users:arr});
+})
+
+app.get('/users/create',function(req,res){
+	res.render('users/create');
+})
 app.listen(port,function(){
 	console.log("Server have localhost is "+port);
 });
