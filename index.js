88e8472dda862f7c797e0d0852405f1ea72f7e69
robocon321 <robocon321n@gmail.com>
2020-01-12 16:14:08 +0700

    Add file index.pug & users.pug

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express=require('express');
 var app=express();
 
-app.get('',function(req,res){
-	res.send("Hello wolrd");
+var port=3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/',function(req,res){
+	res.render('index',{
+		name:"Robocon321#"
+	});
 });
 
-app.listen(3000,function(){
-	console.log("Server have localhost is 3000");
+app.get('/users',function(req,res){
+	res.render('users',{
+		users:[
+			{name:"Hong 10",phone:"0354334442"},
+			{name:"Wing",phone:"0923839923"},
+			{name:"Skim",phone:"0394983959"}
+		]
+	})
+})
+
+app.listen(port,function(){
+	console.log("Server have localhost is "+port);
 });
